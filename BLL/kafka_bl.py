ca5fba1b9074c88fa0176d1f9d68ca0db59802cd
rakefetcster <rakefetcsrer@gmail.com>
2024-11-03 11:53:09 +0200

    adding kafka with data

--- BLL/kafka_bl.py
+++ BLL/kafka_bl.py
@@ -1,4 +1,7 @@
 from DAL.kafka_producer import ProducerDal
+import requests
+from CONFIGURATION.header_key import headers_key
+url = "https://local-business-data.p.rapidapi.com/search"
 
 
 class KafkaBL:
@@ -6,16 +9,27 @@ class KafkaBL:
         self.producer_kafka=ProducerDal('my-community')
         searchUrl = "local-business-data.p.rapidapi.com//search?"
                
-    def send_msg_to_kafka(self,usrObj):
-        # obj_dict = dict()
-        # obj_dict = {"city1":usrObj["city1"],"city2":usrObj["city2"],"city3": usrObj["city3"]}
-        resp = self.producer_kafka.producer_msg(usrObj)
-        return resp
-    
-    # def build_search(self,usrObj):
-    #     query=Cooking%20workshops%20in%20yerushlim%2C%20Israel&limit=20&zoom=13&language=en&region=us&extract_emails_and_contacts=false", headers=headers)
-        
-    #     query=usrObj[activity]+'%20workshops%20in%20yerushlim%2C%20Israel&limit=20&zoom=13&language=en&region=us&extract_emails_and_contacts=false", headers=headers)
+    def send_msg_to_kafka(self,activeObj):
+        list_activity=["Sport","With Kids","With Friends","Food","Romantic","Travel"]
+        activity_dict={
+            "Sport":["Yoga","swimming","dance","football","basketball","Sport"],
+            "With Kids":["Beaches", "Zoos, animal parks, animal sanctuaries","Playgrounds and parks"\
+                         ,"Amusement parks","Water parks","Pedal-boating and rowing"\
+                            ,"Water sports"],
+            "With Friends":["Museum","Go camping","Art Gallery","Volunteer","Plan a picnic","biking"],
+            "Food":["restaurants","food market","Food workshops"],
+            "Romantic":["cooking class", "museum","Hike","movie theater","restaurant","camping"],
+            "Travel":["hiking","camping","Beaches"]
+        }
+        list_paces = ["Tel Aviv-Yafo","Jerusalem","Sea of Galilee","Herzliya","Haifa","Golan","Eilat","Acre","Zikhron Ya'akov"]        
+        print(activity_dict[list_activity[activeObj["activity"]]])
+        for item in activity_dict[list_activity[activeObj["activity"]]]:
+            search = item +" in "+list_paces[activeObj["city"]]+", Israel"
+            querystring = {"query":search,"limit":"20","zoom":"13","language":"en","region":"hb","extract_emails_and_contacts":"false"}
+            print(querystring)
+            headers = headers_key()
+            response = requests.get(url, headers=headers, params=querystring)
 
-
+            resp = self.producer_kafka.producer_msg(response.json())
+            return resp
     
